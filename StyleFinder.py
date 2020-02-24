840ee3bd7e0b42519e2344a119b3d38b946c675d
Nikolaus Van Rompaey <nikolausvanrompaey@Nikolauss-MacBook-Pro.local>
2020-02-24 15:47:46 -0800

    complete un-merged 3-gram functions

--- StyleFinder.py
+++ StyleFinder.py
@@ -21,6 +21,7 @@ class StyleFinder():
                         'RBS','RP','TO','UH','VB','VBD','VBG',
                         'VBN','VBP','VBZ','WDT','WP','WP$','WRB']
         self.postagged = nltk.pos_tag(self.words)
+        self.wordcount = len(self.words)
 
         
     def sentencel_mean(self):
@@ -33,7 +34,7 @@ class StyleFinder():
     
     def lex_diversity(self,size):
         #Take the number of unique tokens per 'size' number of words and divide it by the number of words total.
-        return (len(set(self.words))*size)/len(self.words)
+        return (len(set(self.words))*size)/self.wordcount
  
     def wordl_mean(self):
         #Mean word length
@@ -51,7 +52,7 @@ class StyleFinder():
             - negate_ana_no_k (>find all [NOT word 1, word Central,           ])
             - negate_kata     (>find all [    word 1, word Central, NOT word 3])
             - negate_kata_no_a(>find all [            word Central, NOT word 3])
-            - negate_both     (>find all [NOT word 1, word Central, NOT word 3])
+            - negate_ana_kata (>find all [NOT word 1, word Central, NOT word 3])
             - negate_none     (>find all [    word 1, word Central,     word 3])
             - no_ana          (>find all [            word Central,     word 3])
             - no_kata         (>find all [    word 1, word Central,           ])
@@ -63,7 +64,7 @@ class StyleFinder():
             
     '''
     
-    def super_frequency(self,wordy, ana='_',kata='_',size,cancelvar='gobbledygook'):
+    def super_frequency(self,wordy, ana='_',kata='_',size='1000',cancelvar='gobbledygook'):
         #Takes an 'ana', which is the word before the chosen word, 'wordy', which is the chosen stop word/gerund,
         # and 'kata', which is the word after the chosen word. 'size' follows into the frequency per 'size' number of words.
         # 'cancelvar' is a variable that determines whether ana or kata need canceling, that is...
@@ -110,13 +111,13 @@ class StyleFinder():
         
         #Below chooses the function based on the logic seen above.
         if cancelvar == 'nega':
-            if kata = '_':
+            if kata == '_':
                 counter = negate_ana_no_k(ana,wordy,[a_n,w_n,k_n])
             else:
                 counter = negate_ana(ana,wordy,kata,[a_n,w_n,k_n])
                 
         elif cancelvar== 'negk':
-            if ana = '_':
+            if ana == '_':
                 counter = negate_kata_no_a(wordy,kata,[a_n,w_n,k_n])
             else:
                 counter = negate_kata(ana,wordy,kata,[a_n,w_n,k_n])
@@ -124,14 +125,14 @@ class StyleFinder():
         elif cancelvar == 'negb':
             counter = negate_ana_kata(ana,wordy,kata,[a_n,w_n,k_n])
         elif cancelvar == 'cpos':
-            counter = center_pos(wordy,kata)
+            counter = center_pos(wordy,ana)
 
         else:
-            if ana = '_' and kata = '_':
+            if ana == '_' and kata == '_':
                 counter = center_only(wordy)
-            elif ana = '_':
+            elif ana == '_':
                 counter = no_ana(wordy,kata,[a_n,w_n,k_n])
-            elif kata = '_':
+            elif kata == '_':
                 counter = no_kata(wordy,kata,[a_n,w_n,k_n])
             else:
                 counter = negate_none(ana,wordy,kata,[a_n,w_n,k_n])
@@ -141,68 +142,189 @@ class StyleFinder():
         
     def negate_ana(self,ana,wordy,kata,wak):
         #Takes ana, wordy, and kata, and returns a count of NOT ana, wordy, kata, given the lists.
-        '''FINISH'''
-        return None
+        counter = []
 
+        for i in range(self.wordcount):
+            try:
+                if ( not uplow_switch(self.postagged[i][wak[0]],wak[0]) in ana 
+                and uplow_switch(self.postagged[i+1][wak[1]],wak[1]) in wordy
+                and uplow_switch(self.posttagged[i+2][wak[2]],wak[2]) in kata):
+                    
+                    counter.append(1)
+                    
+                else:
+                    counter.append(0)
+                    
+            except IndexError:
+                counter.append(0)
+
+        return np.sum(counter)
+
+    
     def negate_ana_no_k(self,ana,wordy,wak):
         #Takes ana, wordy, and kata, and returns a count of NOT ana, wordy, given the lists.
-        '''FINISH'''
-        return None
+        counter = []
+
+        for i in range(self.wordcount):
+            try:
+                if ( not uplow_switch(self.postagged[i][wak[0]],wak[0]) in ana 
+                and uplow_switch(self.postagged[i+1][wak[1]],wak[1]) in wordy):
+                    
+                    counter.append(1)
+                    
+                else:
+                    counter.append(0)
+                    
+            except IndexError:
+                counter.append(0)
+
+        return np.sum(counter)
+        
         
     def negate_kata(self,ana,wordy,kata,wak):
         #Takes ana, wordy, and kata, and returns a count of ana, wordy, NOT kata, given the lists.
-        '''FINISH'''
-        return None
+        counter = []
 
+        for i in range(self.wordcount):
+            try:
+                if (uplow_switch(self.postagged[i][wak[0]],wak[0]) in ana 
+                and uplow_switch(self.postagged[i+1][wak[1]],wak[1]) in wordy
+                and not uplow_switch(self.posttagged[i+2][wak[2]],wak[2]) in kata):
+                    
+                    counter.append(1)
+                    
+                else:
+                    counter.append(0)
+                    
+            except IndexError:
+                counter.append(0)
+
+        return np.sum(counter)
+
+    
     def negate_kata_no_a(self,wordy,kata,wak):
         #Takes ana, wordy, and kata, and returns a count of wordy, NOT kata, given the lists.
-        '''FINISH'''
-        return None
+        counter = []
+
+        for i in range(self.wordcount):
+            try:
+                if (uplow_switch(self.postagged[i][wak[1]],wak[1]) in wordy 
+                and not uplow_switch(self.postagged[i+1][wak[2]],wak[2]) in kata):
+                    
+                    counter.append(1)
+                    
+                else:
+                    counter.append(0)
+                    
+            except IndexError:
+                counter.append(0)
+
+        return np.sum(counter)
     
     def negate_ana_kata(self,ana,wordy,kata,wak):
         #Takes ana, wordy, and kata, and returns a count of NOT ana, wordy, NOT kata, given the lists.
-        '''FINISH'''
-        return None
+        counter = []
+
+        for i in range(self.wordcount):
+            try:
+                if (not uplow_switch(self.postagged[i][wak[0]],wak[0]) in ana 
+                and uplow_switch(self.postagged[i+1][wak[1]],wak[1]) in wordy
+                and not uplow_switch(self.posttagged[i+2][wak[2]],wak[2]) in kata):
+                    
+                    counter.append(1)
+                    
+                else:
+                    counter.append(0)
+                    
+            except IndexError:
+                counter.append(0)
+
+        return np.sum(counter)
     
     def negate_none(self,ana,wordy,kata,wak):
         #Takes ana, wordy, and kata, and returns a count of ana, wordy, kata, given the lists.
-        '''FINISH'''
-        return None
+        counter = []
+
+        for i in range(self.wordcount):
+            try:
+                if (uplow_switch(self.postagged[i][wak[0]],wak[0]) in ana 
+                and uplow_switch(self.postagged[i+1][wak[1]],wak[1]) in wordy
+                and uplow_switch(self.posttagged[i+2][wak[2]],wak[2]) in kata):
+                    
+                    counter.append(1)
+                    
+                else:
+                    counter.append(0)
+                    
+            except IndexError:
+                counter.append(0)
+
+        return np.sum(counter)
     
     def no_ana(self,wordy,kata,wak):
         #Takes wordy, and kata, and returns a count of wordy, kata, given the lists.
-        
-        
-        
-        return None    
+        counter = []
+
+        for i in range(self.wordcount):
+            try:
+                if (uplow_switch(self.postagged[i][wak[1]],wak[1]) in wordy 
+                and uplow_switch(self.postagged[i+1][wak[2]],wak[2]) in kata):
+                    
+                    counter.append(1)
+                    
+                else:
+                    counter.append(0)
+                    
+            except IndexError:
+                counter.append(0)
+
+        return np.sum(counter)
+    
 
     def no_kata(self,ana,wordy,wak):
         #Takes wordy, and kata, and returns a count of wordy, kata, given the lists.
         counter = []
-    
-        for i in self.words:
+
+        for i in range(self.wordcount):
             try:
-                if uplow_switch(i[wak[0]],wak[0]) in ana and uplow_switch(i[wak[1],wak[1]]) in wordy:
+                if (uplow_switch(self.postagged[i][wak[0]],wak[0]) in ana 
+                and uplow_switch(self.postagged[i+1][wak[1]],wak[1]) in wordy):
+                    
                     counter.append(1)
+                    
                 else:
                     counter.append(0)
+                    
             except IndexError:
                 counter.append(0)
-                
+
         return np.sum(counter)
     
     def center_only(self,wordy,wak):
         #Takes wordy and returns a count of wordy, given the lists.
         return np.sum([1 if uplow_switch(i[wak],wak) in wordy else 0 for i in self.postagged])
 
+    
+    
 
     def center_pos(self,wordy,postag):
         #Takes wordy, and a POS tag and returns a count of wordy, kata, given the lists.
 
         return np.sum([1 if i[0].lower() in wordy and i[1] in postag else 0 for i in self.postagged])
     
-    def uplow_switch(word,wakval):
+    
+    
+    # Helper Functions Below!
+    def uplow_switch(self,word,wakval):
         if wakval == 0:
             return word.lower()
         else:
             return word.upper()
+    
+    def negator(self,pieces,negval):
+
+        if negval == 0:
+            return not piece
+        else:
+            return piece
+            
